534eeb4319c2275efa00359291ed77554ad49238
Sky Young <sky@alexandria.io>
2018-08-20 16:46:45 -0700

    Add AccountButton

---
+++ src/components/AccountButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { logout } from 'oip-state/src/actions/Account/actions'
+import { accountLogin } from 'oip-state/src/actions/Account/thunks'
+
+class AccountButton extends Component {
+	constructor(props){
+		super(props)
+
+		this.onButtonClick = this.onButtonClick.bind(this)
+	}
+	onButtonClick(){
+		this.props.accountLogin("sarah@gmail.com", "sarah")
+	}
+	render(){
+		let loginText = "Login";
+
+		if (this.props.isLoggedIn)
+			loginText = "Logged In!"
+
+    	return (
+            <button type="button" className="btn btn-outline-dark border-0">Login</button>
+        )
+	}
+}
+
+const mapDispatchToProps = {
+    accountLogin,
+    logout
+}
+
+function mapStateToProps(state) {
+    return {
+        isLoggedIn: state.Account.isLoggedIn,
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountButton)
