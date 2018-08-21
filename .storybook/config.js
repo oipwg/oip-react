f8d22a51009f2a769e41bbc45153f7c1b2a5e129
orpheus <ryanachacon@gmail.com>
2018-08-21 16:31:12 -0700

    + storybook addons -> npm

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import '@storybook/addon-console';
 import { addDecorator } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
 
+
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 function loadStories() {
     require('../stories/index.js')
