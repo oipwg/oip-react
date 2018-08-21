21f6e47cf820813a0bfe641fa85386691817955d
Sky Young <sky@alexandria.io>
2018-08-21 09:44:12 -0700

    Update webpack build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['env', 'react']
+          presets: ['@babel/env', '@babel/react']
         }
       },
       {
