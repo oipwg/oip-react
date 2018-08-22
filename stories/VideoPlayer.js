672e03308386b20a0b96310203cc9e13c7d36609
orpheus <ryanachacon@gmail.com>
2018-08-22 14:01:17 -0700

    minimal story setup with knobs

--- stories/VideoPlayer.js
+++ stories/VideoPlayer.js
@@ -1,48 +1,70 @@
 import React from 'react';
-
+import {Artifact} from 'oip-index'
 import { storiesOf } from '@storybook/react';
 import { withNotes } from '@storybook/addon-notes';
-import { withKnobs, object} from '@storybook/addon-knobs/react';
-
+import { withKnobs, select} from '@storybook/addon-knobs';
 
 import VideoPlayer from '../src/components/VideoPlayer/VideoPlayer'
-import { apocalypse, barbershop, amsterdam } from './TestArtifacts'
+import { apocalypse, barbershop } from './TestArtifacts'
+
+//21252c - apocalypse
+//5533ce - barbershop
 
 const stories = storiesOf('VideoPlayer', module);
-stories.addDecorator(withKnobs)
+stories.addDecorator(withKnobs);
 
-stories.add('width: 300px', withNotes('Using oip-index to pull in a live artifact to pass down as a prop')( () => (
-	<div style={{width: "300px"}}>
-		<VideoPlayer artifact={object("Artifact", apocalypse)} artifactFile={object("ArtifactFile", apocalypse.getFiles()[0])} />
-	</div>
-)));
+const artifact_map = {
+	apocalypse,
+	barbershop
+};
 
-stories.add('width: 600px', withNotes('Using oip-index to pull in a live artifact to pass down as a prop')( () => (
-	<div style={{width: "600px"}}>
-		<VideoPlayer artifact={object("Artifact", apocalypse)} artifactFile={object("ArtifactFile", apocalypse.getFiles()[0])} />
-	</div>
-)));
+const file_map = {
+	apocalypse: {
+		"0": apocalypse.getFiles()[0],
+		"1": apocalypse.getFiles()[1],
+		"2": apocalypse.getFiles()[2]
+	},
+	barbershop: {
+		"0": barbershop.getFiles()[0],
+		"1": barbershop.getFiles()[1],
+		"2": barbershop.getFiles()[2],
+	}
+};
+let artifactKnobLabel = "Artifact";
+let artifactFileKnobLabel = "ArtifactFile";
 
-stories.add('width: 900px', withNotes('Using oip-index to pull in a live artifact to pass down as a prop')( () => (
-	<div style={{width: "900px"}}>
-		<VideoPlayer artifact={object("Artifact", apocalypse)} artifactFile={object("ArtifactFile", apocalypse.getFiles()[0])} />
-	</div>
-)));
+let artifactKnobOptions = {
+	"Apocalypse": "apocalypse",
+	"Barbershop": "barbershop"
+};
 
-stories.add('height: broken', withNotes('Note: this is broken. For height to be set, it has to be done as a property of the object prop, options. Fluid must be set to false for height to take affect.')( () => (
-	<div style={{height: "200px"}}>
-		<VideoPlayer artifact={null} artifactFile={null} />
-	</div>
-)))
+let artifactFileKnobOptions = {
+	"Apocalypse 0": "0",
+	"Apocalypse 1": "1",
+	"Apocalypse 2": "2",
+	"Barbershop 0": "0",
+	"Barbershop 1": "1",
+	"Barbershop 2": "2"
+};
 
-stories.add('live artifact: barbershop', withNotes('Using oip-index to pull in a live artifact to pass down as a prop')( () => (
+let defaultArtifactKnob = "apocalypse";
+let defaultArtifactFileKnob = "0";
+
+
+
+stories.add('Test against multiple artifacts', withNotes('Use knobs to switch between live artifacts')( () => {
+	const artifact_value = select(artifactKnobLabel, artifactKnobOptions, defaultArtifactKnob, "Artifacts");
+	const file_value = select(artifactFileKnobLabel, artifactFileKnobOptions, defaultArtifactFileKnob, "Artifact Files");
+	return (
+		<div style={{width: "500px"}}>
+			<VideoPlayer Artifact={artifact_map[artifact_value]} ArtifactFile={file_map.apocalypse[file_value]} />
+		</div>
+		)
+	}
+));
+
+stories.add('live Artifact: barbershop', withNotes('Using oip-index to pull in a live Artifact to pass down as a prop')( () => (
 	<div style={{width: "500px"}}>
-		<VideoPlayer artifact={object("Artifact", barbershop)} artifactFile={object("ArtifactFile", barbershop.getFiles()[0])} />
-	</div>
-)));
-
-stories.add('live artifact: apocalypse', withNotes('Using oip-index to pull in a live artifact to pass down as a prop')( () => (
-	<div style={{width: "500px"}}>
-		<VideoPlayer artifact={object("Artifact", apocalypse)} artifactFile={object("ArtifactFile", apocalypse.getFiles()[0])} />
+		<VideoPlayer Artifact={barbershop} ArtifactFile={barbershop.getFiles()[0]} />
 	</div>
 )));
