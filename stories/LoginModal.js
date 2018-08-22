124cd33889138c4cb067b108d4bfe592c0a43b7a
Sky Young <sky@alexandria.io>
2018-08-22 16:18:23 -0700

    Add LoginModal Story

---
+++ stories/LoginModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+import { withKnobs, button } from '@storybook/addon-knobs';
+
+import { Provider } from 'react-redux'
+import state from 'oip-state'
+
+import { logout } from 'oip-state/src/actions/Account/actions'
+
+import AccountButton from '../src/components/AccountButton'
+import LoginModal from '../src/components/LoginModal'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import StoreDebugHelper from './StoreDebugHelper'
+
+// Setup Stories
+const stories = storiesOf('LoginModal', module);
+stories.addDecorator(withKnobs)
+
+// Setup Store
+const store = state.createStore()
+
+stories.add('Example', withNotes('')( () => {
+	button("Dispatch Logout", () => { store.dispatch(logout()) })
+	return (
+		<Provider store={store}>
+			<div>
+				<AccountButton />
+				<LoginModal />
+				<hr />
+				<StoreDebugHelper path={['Account', 'showLoginModal']} />
+				<StoreDebugHelper path={['Account', 'showRegisterModal']} />
+				<StoreDebugHelper path={['Account', 'isLoggedIn']} />
+				<StoreDebugHelper path={['Account', 'loginFetching']} />
+				<StoreDebugHelper path={['Account', 'registerFetching']} />
+				<StoreDebugHelper path={['Account', 'Account', '_username']} />
+			</div>
+		</Provider>
+	)
+}));
