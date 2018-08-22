1fb7645a84524b3f63166559a0f103bf463c250a
orpheus <ryanachacon@gmail.com>
2018-08-22 11:33:37 -0700

    added buildIPFS url funcs to util

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-
 let formatPriceString = function(price){
 	// This function assumes the scale has already been applied, and you are passing it a float value
 	var priceStr = parseFloat(price.toFixed(3));
@@ -16,6 +15,30 @@ let formatPriceString = function(price){
 	return priceStr.toString();
 }
 
+const buildIPFSShortURL = (location, fileName) => {
+	if (!location || !fileName)
+		return "";
+
+	return location + "/" + fileName;
+};
+
+const buildIPFSURL = (hash, fname) => {
+	let trailURL = "";
+	if (!fname) {
+		let parts = hash.split('/');
+		if (parts.length == 2) {
+			trailURL = parts[0] + "/" + encodeURIComponent(parts[1]);
+		} else {
+			trailURL = hash;
+		}
+	} else {
+		trailURL = hash + "/" + encodeURIComponent(fname);
+	}
+	return "https://gateway.ipfs.io/ipfs/" + trailURL;
+};
+
 module.exports = {
-	formatPriceString
+	formatPriceString,
+	buildIPFSShortURL,
+	buildIPFSURL
 }
