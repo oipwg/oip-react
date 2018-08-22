5da4fb55ee49d206a59f331dd656c53d40b305bc
orpheus <ryanachacon@gmail.com>
2018-08-22 15:05:39 -0700

    dynamic stories with knobs

--- stories/VideoPlayer.js
+++ stories/VideoPlayer.js
@@ -1,70 +1,49 @@
 import React from 'react';
-import {Artifact} from 'oip-index'
 import { storiesOf } from '@storybook/react';
 import { withNotes } from '@storybook/addon-notes';
 import { withKnobs, select} from '@storybook/addon-knobs';
 
 import VideoPlayer from '../src/components/VideoPlayer/VideoPlayer'
 import { apocalypse, barbershop } from './TestArtifacts'
+import { getArtifactOptions, getFileOptions } from './util'
 
-//21252c - apocalypse
-//5533ce - barbershop
 
 const stories = storiesOf('VideoPlayer', module);
 stories.addDecorator(withKnobs);
 
-const artifact_map = {
-	apocalypse,
-	barbershop
-};
-
-const file_map = {
-	apocalypse: {
-		"0": apocalypse.getFiles()[0],
-		"1": apocalypse.getFiles()[1],
-		"2": apocalypse.getFiles()[2]
-	},
-	barbershop: {
-		"0": barbershop.getFiles()[0],
-		"1": barbershop.getFiles()[1],
-		"2": barbershop.getFiles()[2],
-	}
-};
-let artifactKnobLabel = "Artifact";
-let artifactFileKnobLabel = "ArtifactFile";
-
-let artifactKnobOptions = {
-	"Apocalypse": "apocalypse",
-	"Barbershop": "barbershop"
-};
-
-let artifactFileKnobOptions = {
-	"Apocalypse 0": "0",
-	"Apocalypse 1": "1",
-	"Apocalypse 2": "2",
-	"Barbershop 0": "0",
-	"Barbershop 1": "1",
-	"Barbershop 2": "2"
-};
-
-let defaultArtifactKnob = "apocalypse";
-let defaultArtifactFileKnob = "0";
-
-
+const artifacts = getArtifactOptions([apocalypse, barbershop]);
 
 stories.add('Test against multiple artifacts', withNotes('Use knobs to switch between live artifacts')( () => {
-	const artifact_value = select(artifactKnobLabel, artifactKnobOptions, defaultArtifactKnob, "Artifacts");
-	const file_value = select(artifactFileKnobLabel, artifactFileKnobOptions, defaultArtifactFileKnob, "Artifact Files");
+
+	const artifact_value = select(artifacts.title, artifacts.options, artifacts.default_artifact);
+	const artifact = artifacts.map[artifact_value];
+
+	const artifact_files = getFileOptions(artifact);
+	const file_value = select(artifact_files.title, artifact_files.options, artifact_files.default_file);
+	const artifact_file = artifact_files.map[file_value];
+
 	return (
 		<div style={{width: "500px"}}>
-			<VideoPlayer Artifact={artifact_map[artifact_value]} ArtifactFile={file_map.apocalypse[file_value]} />
+			<VideoPlayer Artifact={artifact} ArtifactFile={artifact_file} />
 		</div>
 		)
 	}
 ));
 
-stories.add('live Artifact: barbershop', withNotes('Using oip-index to pull in a live Artifact to pass down as a prop')( () => (
-	<div style={{width: "500px"}}>
+stories.add('width: 300px', withNotes('Testing responsive design')( () => (
+	<div style={{width: "300px"}}>
+		<VideoPlayer Artifact={barbershop} ArtifactFile={barbershop.getFiles()[0]} />
+	</div>
+)));
+
+stories.add('width: 600px', withNotes('Testing responsive design')( () => (
+	<div style={{width: "600px"}}>
+		<VideoPlayer Artifact={barbershop} ArtifactFile={barbershop.getFiles()[0]} />
+	</div>
+)));
+
+stories.add('width: 900px', withNotes('Testing responsive design')( () => (
+	<div style={{width: "900px"}}>
 		<VideoPlayer Artifact={barbershop} ArtifactFile={barbershop.getFiles()[0]} />
 	</div>
 )));
