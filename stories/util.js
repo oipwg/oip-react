7d2e850ae74e9722b271585c31addbe22b7be062
Sky Young <sky@alexandria.io>
2018-08-22 13:59:21 -0700

    Add util functions

---
+++ stories/util.js
@@ -0,0 +1,53 @@
+let getArtifactOptions = function(artifact_array){
+	let title = "Artifact"
+	let options = {}
+	let map = {}
+	let default_artifact = ""
+
+	for (let i = 0; i < artifact_array.length; i++){
+		let title = artifact_array[i].getTitle()
+
+		options[title] = title
+		map[title] = artifact_array[i]
+
+		if (i === 0)
+			default_artifact = title
+	}
+
+	return {
+		title,
+		options,
+		map,
+		default_artifact
+	}
+}
+
+let getFileOptions = function(Artifact){
+	let title = "ArtifactFiles"
+	let options = {}
+	let map = {}
+	let default_file = ""
+
+	let files = Artifact.getFiles()
+	for (let i = 0; i < files.length; i++){
+		let displayName = files[i].getDisplayName()
+
+		options[displayName] = displayName
+		map[displayName] = files[i]
+
+		if (i === 0)
+			default_file = displayName
+	}
+
+	return {
+		title,
+		options,
+		map,
+		default_file
+	}
+}
+
+module.exports = {
+	getArtifactOptions,
+	getFileOptions
+}
