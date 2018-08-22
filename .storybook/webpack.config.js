403ee555f05deae53dbbb84f9c4f85607d8d2d64
Sky Young <sky@alexandria.io>
2018-08-22 16:01:35 -0700

    Add Webpack Config for Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif|ttf|woff|woff2|eot|svg)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    fs: "empty"
+  }
+};
