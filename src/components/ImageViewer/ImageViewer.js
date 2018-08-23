fce0264985d1dd550c54f36c0c05b28d210e810e
Anthony Howl <anthonyhowl@protonmail.ch>
2018-08-23 12:48:06 -0700

    IPFS connect through Utils

--- src/components/ImageViewer/ImageViewer.js
+++ src/components/ImageViewer/ImageViewer.js
@@ -3,47 +3,29 @@ import { Index } from 'oip-index';
 import PropTypes from 'prop-types';
 import './assets/css/ImageViewer.css'
 import ArtifactFile from 'oip-index/lib/ArtifactFile';
+import { buildIPFSURL, buildIPFSShortURL } from '../../utils.js'
+
 
 class ImageViewer extends Component {
 	constructor(props){
 		super(props)
 
-		this.buildIPFSShortURL = this.buildIPFSShortURL.bind(this);
-		this.buildIPFSURL = this.buildIPFSURL.bind(this);
 	
 	}
 
-	buildIPFSShortURL(location, fileName) {
-        if (!location || !fileName)
-            return "";
-
-        return location + "/" + fileName;
-    }
-
-    buildIPFSURL(hash, fname) {
-        let trailURL = "";
-        if (!fname) {
-            let parts = hash.split('/');
-            if (parts.length == 2) {
-                trailURL = parts[0] + "/" + encodeURIComponent(parts[1]);
-            } else {
-                trailURL = hash;
-            }
-        } else {
-            trailURL = hash + "/" + encodeURIComponent(fname);
-        }
-        return "https://gateway.ipfs.io/ipfs/" + trailURL;
-    }
 	
 	render() {
 		let hash = "";
 		let url = "";
 
 		if (this.props.artifact && this.props.artifactFile) {
-			hash = this.buildIPFSShortURL(this.props.artifact.getLocation(), this.props.artifactFile.getFilename());
-			url = this.buildIPFSURL(hash);
+			hash = buildIPFSShortURL(this.props.artifact.getLocation(), this.props.artifactFile.getFilename());
+			url = buildIPFSURL(hash);
 		}
 		
+		if (!this.props.artifact && this.props.artifactFile) {
+			console.log('Artifact Image was not selected!')
+		}
 
 		return (
 			<img className='OIP-ImageViewer' src={url} alt="OIP-ImageViewer" />
@@ -53,8 +35,8 @@ class ImageViewer extends Component {
 
 ImageViewer.SUPPORTED_FILE_TYPES = ["jpeg", "jpg", "gif", "png", "svg", "bmp", "ico"]
 ImageViewer.propTypes = {
-	artifact: PropTypes.object.isRequired,
-	artifactFile: PropTypes.object.isRequired
+	// artifact: PropTypes.object.isRequired,
+	// artifactFile: PropTypes.object.isRequired
 };
 		
 export default ImageViewer;
