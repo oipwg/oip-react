55276dba1f64f21ee00a3f64306fb62d6ea5d1dd
Sky Young <sky@alexandria.io>
2018-08-23 14:01:32 -0700

    Add PaymentButton to exports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import AccountButton from './components/AccountButton'
 import LoginModal from './components/LoginModal'
 import LoginBlock from './components/LoginBlock'
 import RegisterBlock from './components/RegisterBlock'
+import PaymentButton from './components/PaymentButton'
 
 import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 import ImageViewer from './components/ImageViewer/ImageViewer'
@@ -12,6 +13,7 @@ module.exports = {
 	LoginModal,
 	LoginBlock,
 	RegisterBlock,
+	PaymentButton,
 
 	VideoPlayer,
 	ImageViewer
