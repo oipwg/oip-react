e21dd2accb9e4a80b5fef48f900f29be3c6d46ca
orpheus <ryanachacon@gmail.com>
2018-08-24 09:28:36 -0700

    spacing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import LoginBlock from './components/LoginBlock'
 import RegisterBlock from './components/RegisterBlock'
 import PaymentButton from './components/PaymentButton'
 import CoinbaseModal from './components/CoinbaseModal'
-
 import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 import ImageViewer from './components/ImageViewer/ImageViewer'
 
@@ -16,7 +15,6 @@ module.exports = {
 	RegisterBlock,
 	PaymentButton,
 	CoinbaseModal,
-
 	VideoPlayer,
 	ImageViewer
 }
