748981fff4156cbab999390a4db7e7af2c24b668
Anthony Howl <anthonyhowl@protonmail.ch>
2018-08-24 11:19:40 -0700

    AudioPlayer lockfile

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -8,29 +8,48 @@ import { buildIPFSURL, buildIPFSShortURL } from './../utils.js'
 
 class AudioPlayer extends React.Component {
     constructor(props){
-		super(props)
-
-	
+        super(props)
+        
+        this.preventPlay = this.preventPlay.bind(this)
+    }
+    preventPlay(event){
+        this.react_audio_player.audioEl.pause()
     }
     render() {
 		let hash = "";
-		let url = "";
-            if (this.props.Artifact && this.props.ArtifactFile) {
-                hash = buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.ArtifactFile.getFilename());
-                url = buildIPFSURL(hash);
-            }
-        console.log(url)
+        let url = "";
+        let controls = {}
+        console.log(this.props.lockFile)
+
+        if (this.props.Artifact && this.props.ArtifactFile) {
+            hash = buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.ArtifactFile.getFilename());
+            url = buildIPFSURL(hash);
+        }
+
+        if (this.props.lockFile == true){
+         return (
+             <ReactAudioPlayer 
+             ref={(element) => { this.react_audio_player = element; }}
+             src={url}
+             autoPlay={false}
+             controls={true}
+             onPlay={this.preventPlay}
+             />
+         )           
+        }   
+            
 		return (
 			<ReactAudioPlayer
             src={url}
             autoPlay={false}
-            controls />
+            controls={true} />
 		);
 	}
 }
 AudioPlayer.SUPPORTED_FILE_TYPES = ["mp3", "ogg", "wav"];
 AudioPlayer.propTypes = {
     Artifact: PropTypes.object,
-    ActiveFile: PropTypes.object
+    ActiveFile: PropTypes.object,
+    lockFile: PropTypes.bool
 };
 export default AudioPlayer;
