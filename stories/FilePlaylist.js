dc344e44e82699957dd5be26be03cc9a71b9c574
orpheus <ryanachacon@gmail.com>
2018-08-28 15:16:27 -0700

    test with parent height

--- stories/FilePlaylist.js
+++ stories/FilePlaylist.js
@@ -28,6 +28,23 @@ const widthOptions = {
 };
 const widthDefault = '500px';
 
+const heightLabel = "Parent Div Height";
+const heightOptions = {
+	"100": "100px",
+	"200": "200px",
+	"300": "300px",
+	"400": "400px",
+	"500": "500px",
+	"600": "600px",
+	"700": "700px",
+	"800": "800px",
+	"900": "900px",
+	"1000": "1000px",
+	"1100": "1100px",
+	"100%": "100%"
+};
+const heightDefault = '500px';
+
 const artifacts = getArtifactOptions([apocalypse, barbershop, barbershop_paid, amsterdam, scout, CorMetallicum, dweb, sintel]);
 
 stories.add('Basic FilePlaylist', () => {
@@ -38,10 +55,11 @@ stories.add('Basic FilePlaylist', () => {
 	const file_value = select(artifact_files.title, artifact_files.options, artifact_files.default_file);
 	const artifact_file = artifact_files.map[file_value];
 
-	const width_value = select(widthLabel, widthOptions, widthDefault)
+	const width_value = select(widthLabel, widthOptions, widthDefault);
+	const height_value = select(heightLabel, heightOptions, heightDefault);
 
 	return (
-		<div style={{width: width_value}}>
+		<div style={{width: width_value, height: height_value}}>
 			<FilePlaylist Artifact={artifact} ArtifactFile={artifact_file} />
 		</div>
 	)
