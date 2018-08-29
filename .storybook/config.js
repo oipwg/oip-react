40689f0d546d0f510aa986dc2a2f3e7523df855d
Sky Young <sky@alexandria.io>
2018-08-29 14:10:49 -0700

    Add Specs and Enzyme to use for testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,13 @@ import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { withConsole, setConsoleOptions } from '@storybook/addon-console';
 import { withNotes } from '@storybook/addon-notes';
-
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 addDecorator((storyFn, context) => withConsole({ panelExclude: [/[HMR]/g, /bip32-utils/g, /color/g] })(storyFn)(context));
 addDecorator(withNotes);
+
+Enzyme.configure({ adapter: new Adapter() })
 
 function loadStories() {
     require('../stories/index.js')
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -26,5 +26,12 @@ module.exports = {
 	},
 	node: {
 		fs: "empty"
+	},
+	externals: {
+		'jsdom': 'window',
+		'cheerio': 'window',
+		'react/lib/ExecutionEnvironment': true,
+		'react/lib/ReactContext': 'window',
+		'react/addons': true,
 	}
 };
