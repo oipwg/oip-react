e2099d757f5abd7920405b8c7ef04cd795996714
orpheus <ryanachacon@gmail.com>
2018-08-28 17:03:03 -0700

    adding responsive and inline styles

--- src/components/FilePlaylist/Playlist.js
+++ src/components/FilePlaylist/Playlist.js
@@ -4,7 +4,7 @@ import { getFileExtension } from '../../utils.js'
 
 class Playlist extends Component {
 	render() {
-		let files = [];
+		let files = [], artist;
 		if (this.props.Artifact) {
 			let tmpFiles = this.props.Artifact.getFiles();
 			let supportedFileTypes = ["mp4", "mp3", "ogg", "wav"];
@@ -14,6 +14,9 @@ class Playlist extends Component {
 					files.push(f)
 				}
 			}
+		}
+		if (this.props.Artifact) {
+			artist = this.props.Artifact.getDetail("artist")
 		}
 		return (
 			<ul className="list-group">
@@ -27,7 +30,23 @@ class Playlist extends Component {
 				{files.map(function(file, i){
 					return (
 						<li key={i} className="list-group-item">
-							<div style={{height: "50px"}}>{file.getFilename()}</div>
+							<div className="row no-gutters" style={{height: "50px"}}>
+								<div className="col-1 d-flex align-items-center"><span style={{color: "#999"}}> {i} </span></div>
+								<div className="col-11">
+									<div className="row no-gutters pt-1">
+										<div className="col d-flex justify-content-start ">
+											<span className="mr-2" style={{color: "#999"}}>{artist ? artist : "unknown creator"}</span>
+											<span className="ml-2" style={styles.filename}>{file.getFilename()}</span>
+										</div>
+									</div>
+									<div className="row no-gutters mt-2">
+										<div className="col d-flex justify-content-start align-items-center">
+											Holder
+										</div>
+									</div>
+
+								</div>
+							</div>
 						</li>
 					)
 				})}
@@ -37,3 +56,12 @@ class Playlist extends Component {
 }
 
 export default Playlist;
+
+const styles ={
+	filename: {
+		textOverflow: "ellipsis",
+		overflow: "hidden",
+		whiteSpace: "nowrap",
+		color: "black"
+	}
+};
