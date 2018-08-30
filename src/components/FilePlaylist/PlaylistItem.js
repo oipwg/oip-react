a9e2f4c3f7a7af6bbe97eec3118a99e01513af00
orpheus <ryanachacon@gmail.com>
2018-08-30 13:16:01 -0700

    set and unset active files

--- src/components/FilePlaylist/PlaylistItem.js
+++ src/components/FilePlaylist/PlaylistItem.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 import PaymentButton from '../PaymentButton'
-
 import { getIPFSImage} from '../../utils'
+import { fileToUID, setActiveFile } from 'oip-state/src/actions/ActiveArtifactFiles/thunks';
 import './assets/FilePlaylist.css'
-
 
 class PlaylistItem extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.uid = fileToUID(this.props.File);
 		this.state = {
 			active: false
+		};
+
+		this.setActiveFile = this.setActiveFile.bind(this);
+
+	}
+
+	setActiveFile() {
+		let activeFiles = this.props.ActiveArtifactFiles;
+		if (activeFiles.active === this.uid) {
+			this.props.setActiveFile(undefined)
+		} else {
+			for (let uid in activeFiles) {
+				if (uid === this.uid) {
+					this.props.setActiveFile(activeFiles[uid].ArtifactFile)
+				}
+			}
 		}
 	}
+
+	isActive() {
+		return this.uid === this.props.ActiveArtifactFiles.active;
+	}
 	render() {
-		let loadWithPicture = false, artifact, poster, artist, filename, file;
+		let loadWithPicture = true, artifact, poster, artist, filename, file;
 		if (this.props.File) {
 			file = this.props.File;
 			artifact = file.parent;
@@ -23,7 +46,7 @@ class PlaylistItem extends React.Component {
 			filename = file.getFilename()
 		}
 		return (
-			<div className="row no-gutters file-playlist-item" onClick={() => {console.log("Item click")}}>
+			<div className={`row no-gutters file-playlist-item ${this.isActive() ? "isActive" : null}`} onClick={this.setActiveFile}>
 				{/*img*/}
 				<div className={loadWithPicture ? "col-2 d-flex align-items-center playlist-artifact-image" : "d-none"} >
 					<img className="img-responsive" style={{maxHeight: '38px'}} src={poster} alt='poster' />
@@ -47,10 +70,12 @@ class PlaylistItem extends React.Component {
 							<PaymentButton
 								ArtifactFile={file}
 								type="view"
+								style={styles.paymentButton}
 							/>
 							<PaymentButton
 								ArtifactFile={file}
 								type="buy"
+								style={styles.paymentButton}
 							/>
 						</div>
 					</div>
@@ -67,6 +92,26 @@ const styles = {
 		textOverflow: 'ellipsis',
 		wordBreak: 'normal'
 	},
+	paymentButton: {
+		fontSize: "12px",
+		border: "none",
+	}
 };
 
-export default PlaylistItem;
+PlaylistItem.propTypes = {
+	File: PropTypes.object,
+	index: PropTypes.number
+};
+
+function mapStateToProps(state) {
+	return {
+		state: state,
+		ActiveArtifactFiles: state.ActiveArtifactFiles
+	}
+}
+
+const mapDispatchToProps = {
+	setActiveFile
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlaylistItem);
