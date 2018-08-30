60975932c9228691502b7522a35d3731a6171d02
Anthony Howl <anthonyhowl@protonmail.ch>
2018-08-30 16:41:45 -0700

    Pre-Test + Sinon.js

--- stories/ImageViewer.js
+++ stories/ImageViewer.js
@@ -4,7 +4,11 @@ import { withNotes } from '@storybook/addon-notes';
 import { withKnobs, object, select, boolean } from '@storybook/addon-knobs';
 import ImageViewer from '../src/components/ImageViewer/ImageViewer.js'
 import { getFileOptions, getArtifactOptions} from './util.js'
+import { action } from '@storybook/addon-actions';
 import { amsterdam, apocalypse, barbershop, scout } from './TestArtifacts'
+import { specs, describe, it } from 'storybook-addon-specifications'
+import {mount} from "enzyme";
+import expect from "expect";
 
 const stories = storiesOf('ImageViewer', module);
 stories.addDecorator(withKnobs)
@@ -12,16 +16,13 @@ stories.addDecorator(withNotes)
 
 const artifacts = getArtifactOptions([amsterdam, scout])
 
-stories.add('Knobs', () => {
-
-	
+stories.add('IV-Test', () => {
 	const width_value = select("Parent: div.width", {
 		"300px": "300px",
 		"500px": "500px",
 		"900px": "900px"
 
 	},"300px")
-
 
 	const artifact_value = select(artifacts.title, artifacts.options, artifacts.default_artifact)
 	const artifact = artifacts.map[artifact_value]
@@ -32,9 +33,37 @@ stories.add('Knobs', () => {
 
 	const artifact_file = artifact_files.map[file_value]
 
-	return (
+	const story = 
 		<div style={{width: width_value}}>
 			<ImageViewer Artifact={artifact} ArtifactFile={artifact_file} lockFile={boolean("Lock File", false)} />
 		</div>
-	)
-}, { notes: 'Passing kobs' })
+
+	specs(() => describe("Image Viewer", () => {
+		it("Should fit to parent div width", (done) => {
+			// const story = 
+			// 	<div style={{width: "200px"}}>
+			// 		<ImageViewer Artifact={amsterdam} ArtifactFile={amsterdam.getFiles()[0]} />
+			// 	</div>
+			
+			let output = mount(story)
+
+			console.log(output.find("canvas"))
+			console.log(output.find("canvas").getDOMNode())
+
+			expect(output.find("canvas").getDOMNode().width).toBe(300)
+			expect(output.find("canvas").getDOMNode().height).toBe(225)
+		});
+		// it("Should blur on lockFile", () => {
+		// 	const story = 
+		// 		<div style={{width: "200px"}}>
+		// 			<ImageViewer lockFile={true} />
+		// 		</div>
+			
+		// 	let output = mount(story)
+		// 	console.log(output.find("canvas"))
+		// 	// expect(output.find("canvas")).toContain()
+		// });
+	}));
+
+	return story
+})
