640719bcfd233444a49e474e26adcfbdddf51652
Anthony Howl <anthonyhowl@protonmail.ch>
2018-08-31 11:54:37 -0700

    Blur on lockfile test for IV

--- stories/ImageViewer.js
+++ stories/ImageViewer.js
@@ -33,36 +33,16 @@ stories.add('IV-Test', () => {
 
 	const artifact_file = artifact_files.map[file_value]
 
-	const story = 
-		<div style={{width: width_value}}>
-			<ImageViewer Artifact={artifact} ArtifactFile={artifact_file} lockFile={boolean("Lock File", false)} />
-		</div>
-
-	specs(() => describe("Image Viewer", () => {
-		it("Should fit to parent div width", (done) => {
-			// const story = 
-			// 	<div style={{width: "200px"}}>
-			// 		<ImageViewer Artifact={amsterdam} ArtifactFile={amsterdam.getFiles()[0]} />
-			// 	</div>
-			
-			let output = mount(story)
-
-			console.log(output.find("canvas"))
-			console.log(output.find("canvas").getDOMNode())
-
-			expect(output.find("canvas").getDOMNode().width).toBe(300)
-			expect(output.find("canvas").getDOMNode().height).toBe(225)
+	const story = 	<div style={{width: width_value}}>
+						<ImageViewer Artifact={artifact} ArtifactFile={artifact_file} lockFile={boolean("Lock File", false)} />
+					</div>
+		
+			specs(() => describe("<ImageViewer />", () => {
+				it("Blur on lockfile", () => {
+					const wrapper = mount(story)
+					wrapper.setProps({lockFile: true})
+				expect(wrapper.props().lockFile).toBe(true);
 		});
-		// it("Should blur on lockFile", () => {
-		// 	const story = 
-		// 		<div style={{width: "200px"}}>
-		// 			<ImageViewer lockFile={true} />
-		// 		</div>
-			
-		// 	let output = mount(story)
-		// 	console.log(output.find("canvas"))
-		// 	// expect(output.find("canvas")).toContain()
-		// });
 	}));
 
 	return story
