1015dee054a154160252f9a46d6b5a2cf5ef8469
orpheus <ryanachacon@gmail.com>
2018-09-04 16:32:19 -0700

    add test artifact

--- stories/ImageViewer.js
+++ stories/ImageViewer.js
@@ -5,7 +5,7 @@ import { withKnobs, object, select, boolean } from '@storybook/addon-knobs';
 import ImageViewer from '../src/components/ImageViewer/ImageViewer.js'
 import { getFileOptions, getArtifactOptions} from './util.js'
 import { action } from '@storybook/addon-actions';
-import { amsterdam, apocalypse, barbershop, scout } from './TestArtifacts'
+import { amsterdam, apocalypse, barbershop, scout, CorMetallicum } from './TestArtifacts'
 import { specs, describe, it } from 'storybook-addon-specifications'
 import {mount} from "enzyme";
 import expect from "expect";
@@ -15,24 +15,22 @@ const stories = storiesOf('ImageViewer', module);
 stories.addDecorator(withKnobs);
 stories.addDecorator(withNotes);
 
-const artifacts = getArtifactOptions([amsterdam, scout]);
+const artifacts = getArtifactOptions([amsterdam, scout, CorMetallicum]);
 
 stories.add('IV-Test', () => {
+	const artifact_value = select(artifacts.title, artifacts.options, artifacts.default_artifact);
+	const artifact = artifacts.map[artifact_value];
+
+	const artifact_files = getFileOptions(artifact);
+	const file_value = select(artifact_files.title, artifact_files.options, artifact_files.default_file);
+	const artifact_file = artifact_files.map[file_value];
+
 	const width_value = select("Parent: div.width", {
 		"300px": "300px",
 		"500px": "500px",
 		"900px": "900px"
 
-	},"300px");
-
-	const artifact_value = select(artifacts.title, artifacts.options, artifacts.default_artifact);
-	const artifact = artifacts.map[artifact_value];
-
-	const artifact_files = getFileOptions(artifact);
-
-	const file_value = select(artifact_files.title, artifact_files.options, artifact_files.default_file);
-
-	const artifact_file = artifact_files.map[file_value];
+	}, "300px");
 
 	const story = 	
 		<div style={{width: width_value}}>
