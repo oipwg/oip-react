d809c8077f7c642c75ca9dd83af6758bf7674314
orpheus <ryanachacon@gmail.com>
2018-09-04 16:51:06 -0700

    create image wrapper

---
+++ src/components/ImageViewer/PosterWrapper.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ImageViewer from "./ImageViewer";
+
+class PosterWrapper extends Component {
+	render() {
+		let af = this.props.ArtifactFile;
+		let posterFile;
+		if (af) {
+			let artifact = af.parent;
+			if (artifact.getThumbnail()) {
+				posterFile = artifact.getThumbnail()
+			}
+
+		}
+		return <ImageViewer ArtifactFile={posterFile}/>
+	}
+}
+
+export default PosterWrapper;
