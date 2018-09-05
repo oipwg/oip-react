6d01a2907c0503f587773d4047cde89c155c6c81
orpheus <ryanachacon@gmail.com>
2018-09-05 09:09:51 -0700

    grab color palette

--- src/components/AudioViewer/AudioViewer.js
+++ src/components/AudioViewer/AudioViewer.js
@@ -1,21 +1,38 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux'
+import ColorThief from 'color-thief'
 import { PlayButton, PauseButton } from 'react-player-controls'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
-import {getIPFSURL} from "../../utils";
+import {getIPFSURL, getIPFSImage} from "../../utils";
 import AudioWaveSurfer from './AudioWaveSurfer';
+import PosterWrapper from '../ImageViewer/PosterWrapper'
 import './assets/styles/AudioViewer.scss'
 
 class AudioViewer extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
-
+		this.state = {
+			colorOne: '#fff',
+			colorTwo: '#000',
+			colorThree: '#000'
+		};
 	}
+
+	generateColorPalette(imageNode) {
+		console.log("THIS: ", this.state)
+		console.log("IMAGE NODE: ", imageNode);
+		let colorThief = new ColorThief();
+		let palette = colorThief.getPalette(imageNode, 2);
+		console.log("Palette: ", palette);
+		// this.setState({colorOne: palette[0]})
+		// this.setState({colorTwo: palette[1]})
+		// this.setState({colorThree: palette[2]})
+	}
+
 	render() {
 		let file = this.props.ArtifactFile || this.props.ReduxArtifactFile;
 		let url, artist, title;
@@ -27,23 +44,34 @@ class AudioViewer extends Component {
 			artist = "unknown";
 			title = "unknown";
 		}
-		return (
-			<div className="AudioViewer-container container-fluid">
-				<div className="d-flex">
-					<div>
-						<div>
-							<PlayButton
-								isEnabled={true}
-								onClick={ () => {console.log("Oh hello")}}
-							/>
-						</div>
 
-						<div>
+		//@ToDO: add redux variables for play state
+		let isPlaying = false;
+		const playbackButton = isPlaying ? (
+			<PauseButton
+				isEnabled={true}
+				onClick={() => {console.log("Pause")}}
+			/>) : (
+			<PlayButton
+				isEnabled={true}
+				onClick={() => {console.log("Play")}}
+			/>);
+
+		return (
+			<div className="AudioViewer-container container-fluid" style={{height: "100%", width: "100%"}}>
+				<div className="d-flex no-gutters">
+					<div className="">
+						{playbackButton}
+					</div>
+					<div className="">
 							<div>{artist}</div>
 							<div>{title}</div>
+					</div>
+					<div className="ml-auto">
+						<div style={{height: '300px', width: '300px'}}>
+							<PosterWrapper ArtifactFile={file} onImageLoad={this.generateColorPalette}/>
 						</div>
 					</div>
-					<div>canvas/album art</div>
 				</div>
 				<div>
 					<AudioWaveSurfer ArtifactFile={file}/>
