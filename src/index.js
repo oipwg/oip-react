9586f16d6787426224266140b47b3eb2ab93b774
Sky Young <sky@alexandria.io>
2018-09-10 10:57:57 -0700

    Export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Account Components
+// Account Items
 import AccountButton from './components/AccountButton/AccountButton'
 import LoginModal from './components/LoginModal/LoginModal'
 import LoginBlock from './components/LoginBlock/LoginBlock'
@@ -12,29 +12,29 @@ import ArtifactTitle from './components/ArtifactPieces/pieces/ArtifactTitle'
 import ArtifactDescription from './components/ArtifactPieces/pieces/ArtifactDescription'
 import ArtifactArtist from './components/ArtifactPieces/pieces/ArtifactArtist'
 
-// ArtifactFile Components
+// File Components
 import FilePaymentWrapper from './components/FilePaymentWrapper'
 import FileViewer from './components/FileViewer'
 import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 import ImageViewer from './components/ImageViewer/ImageViewer'
 import PosterWrapper from './components/ImageViewer/PosterWrapper'
 
-// Export Account Components
-export AccountButton
-export LoginModal
-export RegisterBlock
-export PaymentButton
-export CoinbaseModal
-export CoinbaseWrapper
+module.exports = {
+	AccountBbutton,
+	LoginModal,
+	LoginBlock,
+	RegisterBlock,
+	PaymentButton,
+	CoinbaseModal,
+	CoinbaseWrapper,
 
-// Export Artifact Components
-export ArtifactTitle
-export ArtifactDescription
-export ArtifactArtist
+	ArtifactTitle,
+	ArtifactDescription,
+	ArtifactArtist,
 
-// Export ArtifactFile Components
-export FilePaymentWrapper
-export FileViewer
-export VideoPlayer
-export ImageViewer
-export PosterWrapper
+	FilePaymentWrapper,
+	FileViewer,
+	VideoPlayer,
+	ImageViewer,
+	PosterWrapper
+}
