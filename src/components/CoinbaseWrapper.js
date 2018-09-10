9e51f06ec72d232df7c807c9e0437ba8d79c27da
Sky Young <sky@alexandria.io>
2018-09-10 16:22:23 -0700

    Add Pending Coinbase Payment

--- src/components/CoinbaseWrapper.js
+++ src/components/CoinbaseWrapper.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
+import { PropagateLoader } from 'react-spinners'
+
 import CoinbaseModal from './CoinbaseModal'
 
 import { handleCoinbaseModalEvents } from "oip-state/src/actions/Payment/thunks";
@@ -10,31 +13,35 @@ class CoinbaseWrapper extends Component {
 		super(props)
 	}
 	render(){
-		let loginText = "Login";
+		return (
+			<div>
+				{ this.props.Payment.showCoinbaseModal ? 
+					<CoinbaseModal 
+						isOpen={this.props.Payment.showCoinbaseModal}
+						currency={this.props.info.currency}
+						address={this.props.info.address}
+						amount={this.props.info.amount}
 
-		if (this.props.isLoggedIn){
-			if (this.props.Account && this.props.Account._username)
-				loginText = this.props.Account._username
-			else
-				loginText = "Logged In"
-		}
-
-		if (this.props.Payment && this.props.Payment.showCoinbaseModal){
-			return (
-				<CoinbaseModal 
-					isOpen={this.props.Payment.showCoinbaseModal}
-					currency={this.props.info.currency}
-					address={this.props.info.address}
-					amount={this.props.info.amount}
-
-					onClose={() => { this.props.handleCoinbaseModalEvents("close") } }
-					onCancel={() => { this.props.handleCoinbaseModalEvents("cancel") } }
-					onSuccess={() => { this.props.handleCoinbaseModalEvents("success") } }
-				/>
-			)
-		} else {
-			return null
-		}
+						onClose={() => { this.props.handleCoinbaseModalEvents("close") } }
+						onCancel={() => { this.props.handleCoinbaseModalEvents("cancel") } }
+						onSuccess={() => { this.props.handleCoinbaseModalEvents("success") } }
+					/> 
+					: null
+				}
+				{ this.props.Payment.coinbasePending ? 
+					<Modal isOpen={this.props.Payment.coinbasePending} size={"sm"}>
+						<ModalBody style={{margin: "auto"}} className="text-center">
+							<h5 style={{paddingBottom: "15px"}}>Please wait...</h5>
+							<div style={{margin: "auto", width: "1px", paddingRight: "20px"}}>
+								<PropagateLoader size={20} />
+							</div>
+							<p style={{marginTop: "45px", marginBottom: "0px"}}>Waiting for Coinbase to send...</p>
+						</ModalBody>
+					</Modal> 
+					: null
+				}
+			</div>
+		)
 	}
 }
 
