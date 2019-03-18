5adfa67a2870fa541d0aea85fadb8b3b062ddecd
orpheus <ryanachacon@gmail.com>
2019-03-18 13:11:29 -0700

    import compnent from main path

--- stories/DynamicComplexSearch.stories.js
+++ stories/DynamicComplexSearch.stories.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import DCS from '../components/DynamicComplexSearch/DynamicComplexSearch'
+import { DynamicComplexSearch } from '../components'
 
 storiesOf('DynamicComplexSearch', module)
   .add('with oip-mainnet mapping', () => {
-    return <DCS />
+    return <DynamicComplexSearch />
   })
