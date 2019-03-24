c99452ba993fae2a09f89bec7384df134abc783d
orpheus <ryanachacon@gmail.com>
2019-03-24 08:35:02 -0700

    test overriding/extending styles from story

--- stories/DynamicComplexSearch.stories.js
+++ stories/DynamicComplexSearch.stories.js
@@ -2,9 +2,25 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { DynamicComplexSearch } from '../src/components'
 
+const styles = {
+  selectBase: {
+    borderBottom: '1px solid blue',
+    '&:hover': {
+      backgroundColor: 'red'
+    }
+  },
+  inputQuery: {
+    'border-bottom': '1px solid orange'
+  },
+  buttonBase: {
+    borderRadius: 40
+  }
+}
 storiesOf('DynamicComplexSearch', module)
   .add('with oip-mainnet mapping', () => {
     return <DynamicComplexSearch
+      styles={styles}
       onSubmit={(query) => console.log('my query,', query)}
+      overrideStyles
     />
   })
