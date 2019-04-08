5fabc4e6ef73dc64ac428259137271294a3580c0
orpheus <ryanachacon@gmail.com>
2019-04-08 12:40:09 -0700

    remove theme switch button from tagsinput story

--- stories/TagsInput.stories.js
+++ stories/TagsInput.stories.js
@@ -4,12 +4,9 @@ import { TagsInput } from '../src/components'
 import { useTheme, ThemeProvider } from '../src/theme'
 
 const ThemeWrapper = (props) => {
-  const { theme, changeTheme } = useTheme()
+  const { theme } = useTheme()
   return <ThemeProvider theme={theme}>
     {props.children}
-    <button
-      style={{ display: 'block', marginTop: '30px', backgroundColor: 'unset', border: 'none', padding: '10px', cursor: 'pointer' }}
-      onClick={() => changeTheme(theme.name === 'light' ? 'dark' : 'light')}>Toggle Theme</button>
   </ThemeProvider>
 }
 
