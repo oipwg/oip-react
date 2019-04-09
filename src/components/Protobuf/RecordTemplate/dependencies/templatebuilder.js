83295b09d56f94838944fb406b90361799771101
orpheus <ryanachacon@gmail.com>
2019-04-09 10:21:44 -0700

    push clean function

--- src/components/Protobuf/RecordTemplate/dependencies/templatebuilder.js
+++ src/components/Protobuf/RecordTemplate/dependencies/templatebuilder.js
@@ -1,8 +1,8 @@
 import protobuf from 'protobufjs'
 
 async function templatebuilder (name, description, descriptor) {
-  let ROOT = await protobuf.load(require('./protofiles/compiled/compiled'))
-  console.log(ROOT)
+  // let ROOT = await protobuf
+  // console.log(ROOT)
   //
   // let RecordTemplate = ROOT.lookupType('oip5.record.RecordTemplateProto')
   // console.log(RecordTemplate)
