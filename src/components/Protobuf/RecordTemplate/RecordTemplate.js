49973949165094354fd8e78c4ed1ead75d1cd2b4
orpheus <ryanachacon@gmail.com>
2019-04-10 11:38:07 -0700

    default styled form for building record template (without publishing)

--- src/components/Protobuf/RecordTemplate/RecordTemplate.js
+++ src/components/Protobuf/RecordTemplate/RecordTemplate.js
@@ -1,17 +1,54 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import withStyles from 'react-jss'
 
 import styles from './styles'
 import { DescriptorSetProto } from '../index'
-import {templatebuilder} from './dependencies'
+import { templatebuilder } from './dependencies'
+import { isValidWIF } from '../../../util'
 
 const RecordTemplate = ({ classes }) => {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [privateKey, setPrivateKey] = useState('')
+  const [disableSubmit, toggleDisable] = useState(true)
+  const [descriptor, setProtoDescriptor] = useState(undefined)
   
-  const [name, setName] = useState('test')
-  const [description, setDescription] = useState('test')
+  useEffect(() => {
+    toggleDisable(!isValidWIF(privateKey, 'testnet'))
+  }, [privateKey])
   
-  const handleBuild = (descriptor) => {
-    templatebuilder(name, description, descriptor)
+  const handlePrivateKey = (e) => {
+    setPrivateKey(e.target.value)
+  }
+  
+  const handlePublish = async () => {
+    let template
+    try {
+      template = templatebuilder({
+        friendlyName: name,
+        DescriptorSetProto: descriptor,
+        wif: privateKey,
+        description,
+        network: 'testnet'
+      })
+    } catch (err) {
+      throw Error(err)
+    }
+    
+    if (template) {
+      const prefix = 'p64:'
+      const message = `${prefix}:${template}`
+      
+      try {
+        // let res = await new OIP(...).sendToChain(message)
+      } catch (err) {
+        throw new Error(`failed to send template message to chain: ${err}`)
+      }
+    }
+  }
+  
+  const getProtoDescriptor = (descriptor) => {
+    setProtoDescriptor(descriptor)
   }
   
   return <div className={classes.root}>
@@ -32,8 +69,21 @@ const RecordTemplate = ({ classes }) => {
       />
     </div>
     <DescriptorSetProto
-      onBuild={(descriptor) => handleBuild(descriptor)}
+      getDescriptor={getProtoDescriptor}
     />
+    <div>
+      <span>Input private key (WIF)</span>
+      <input
+        type={'text'}
+        onChange={handlePrivateKey}
+        value={privateKey}
+      />
+    </div>
+    <button
+      disabled={disableSubmit}
+      onClick={handlePublish}
+    >Create & Publish
+    </button>
   </div>
 }
 
