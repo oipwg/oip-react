2b3381ec631072b293722870152e66f03cff53ee
orpheus <ryanachacon@gmail.com>
2019-05-14 14:49:53 -0700

    move comp directory

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import DynamicComplexSearch from './DynamicComplexSearch'
 import { DescriptorSetProto, RecordTemplate } from './Protobuf'
 import { TagsInput } from './UI'
-import { ModuleWallet } from './Wallet'
+import { ModuleWallet } from './ModuleWallet'
 
 export {
   DynamicComplexSearch,
