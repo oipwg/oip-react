128c098dea3282601cdceda16b9bd0a547981a7b
orpheus <ryanachacon@gmail.com>
2019-05-28 15:00:26 -0700

    update record publisher

--- src/components/Protobuf/RecordProto/RecordProto.js
+++ src/components/Protobuf/RecordProto/RecordProto.js
@@ -1,8 +1,11 @@
-import React, { useMemo, useReducer } from 'react'
+import React, { useEffect, useMemo, useReducer, useState } from 'react'
 import withStyles from 'react-jss'
 import PropTypes from 'prop-types'
 import { decodeDescriptor } from 'oip-protobufjs'
+
 import { TagsInput } from '../../UI'
+import WalletButton from '../../WalletButton/WalletButton'
+import { recordProtoBuilder } from 'oip-protobufjs/src'
 
 const fieldHeight = 25
 const fieldWidth = 250
@@ -40,6 +43,9 @@ const styles = theme => ({
     borderRadius: 3,
     fontSize: 12
   },
+  publishContainer: {
+    width: fieldWidth
+  },
   fieldContainer: {
     position: 'relative'
   },
@@ -47,6 +53,26 @@ const styles = theme => ({
     fontSize: 10,
     color: `${theme.palette.greyscale(0.8)}`,
     position: 'absolute'
+  },
+  publishRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  networkSelect: {
+    height: fieldHeight,
+    boxSizing: 'border-box',
+    padding: 4,
+    background: 'none',
+    border: `1px solid ${theme.palette.greyscale(0.3)}`,
+    borderRadius: 3,
+    fontSize: 12,
+    width: 123
+  },
+  walletButton: {
+    '& > button': {
+      padding: [4, 8]
+    }
   },
   // tags input
   tagsInputRoot: {
@@ -56,51 +82,102 @@ const styles = theme => ({
     marginBottom: 10,
     border: `1px solid ${theme.palette.greyscale(0.3)}`,
     borderRadius: 3,
-    height: fieldHeight
+    height: fieldHeight,
+    position: 'relative',
+    '& > input': {
+      width: fieldWidth,
+      position: 'relative',
+      boxSizing: 'border-box'
+    }
   },
   input: {
     width: fieldWidth,
     fontSize: 12,
     '&::placeholder': {
       fontSize: 10,
-    }
+    },
+    boxSizing: 'border-box'
   }
 })
 
 const RecordProto = ({
   classes,
   descriptor,
-  templateName
+  templateName,
+  onSuccess,
+  onError,
+  mainnetExplorerUrl = 'https://livenet.flocha.in/api',
+  testnetExplorerUrl = 'https://testnet.explorer.mediciland.com/api'
 }) => {
   const memoizedDescriptor = useMemo(() => decodeDescriptor(descriptor, true), [descriptor])
   const { webFmt } = memoizedDescriptor
 
   let initialState = {}
 
-  // function init (initialState) {
-  //   let
-  // }
+  function reducer (state, action) {
+    if (action.type === 'UPDATE') {
+      return {
+        ...state,
+        [action.field]: action.value
+      }
+    } else throw Error('Invalid type passed to reducer')
+  }
 
-  // function reducer (state, action) {
-  //   if (action.type === 'UPDATE') {
-  //       return {
-  //         ...state,
-  //         [action.field]: action.value
-  //       }
-  //   } else throw Error('Invalid type passed to reducer')
-  // }
-  //
-  // const [state, dispatch] = useReducer(reducer, initialState, init)
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  function prefixMessage (message) {
+    return `p64:${message}`
+  }
+
+  function serializeState(state) {
+    return {
+      name: templateName,
+      descriptor: descriptor,
+      payload: state
+    }
+  }
+
+  function getMessage ({wif, network}) {
+    // build record template
+    const serializedDetailsData = serializeState(state)
+    let signedMessage
+    try {
+      signedMessage = recordProtoBuilder({
+        detailsData: serializedDetailsData,
+        wif,
+        network
+      })
+    } catch (err) {
+      throw Error(err)
+    }
+    return prefixMessage(signedMessage.signedMessage64)
+  }
+
+  // useEffect(() => {
+  //   getMessage()
+  // }, [state])
 
   return <RecordInterface
     classes={classes}
     webFmt={webFmt}
+    dispatch={dispatch}
+    onSuccess={onSuccess}
+    onError={onError}
+    getMessage={getMessage}
+    mainnetExplorerUrl={mainnetExplorerUrl}
+    testnetExplorerUrl={testnetExplorerUrl}
   />
 }
 
 const RecordInterface = ({
   classes,
-  webFmt
+  webFmt,
+  dispatch,
+  onSuccess,
+  onError,
+  getMessage,
+  mainnetExplorerUrl,
+  testnetExplorerUrl
 }) => {
   return <div className={classes.root}>
     {Object.keys(webFmt.fields).map((field, i) => {
@@ -110,6 +187,7 @@ const RecordInterface = ({
         field={field}
         fieldData={fieldData}
         classes={classes}
+        dispatch={dispatch}
       />
     })}
     {Object.keys(webFmt.enums).map((enumField, i) => {
@@ -119,8 +197,17 @@ const RecordInterface = ({
         enumField={enumField}
         enumData={enumData}
         classes={classes}
+        dispatch={dispatch}
       />
     })}
+    <Publisher
+      classes={classes}
+      onSuccess={onSuccess}
+      onError={onError}
+      getMessage={getMessage}
+      mainnetExplorerUrl={mainnetExplorerUrl}
+      testnetExplorerUrl={testnetExplorerUrl}
+    />
   </div>
 }
 
@@ -128,21 +215,36 @@ const EnumRow = ({
   enumField,
   enumData,
   i,
-  classes
+  classes,
+  dispatch
 }) => {
-  const { values, rule } = enumData
+  const [state, setState] = useState(0)
+
+  function handleSelectChange (e) {
+    setState(Number(e.target.value))
+  }
+
+  useEffect(() => {
+    dispatch({
+      type: 'UPDATE',
+      value: state,
+      field: enumField
+    })
+  }, [state])
+
+  const { values } = enumData // currently don't allow repeated
   return <div className={classes.fieldContainer} key={i}>
     <span className={classes.fieldTitle}>
       Field: {enumField}
     </span>
     <select
-      value={0}
-      onChange={() => {}}
+      value={state}
+      onChange={handleSelectChange}
       className={classes.selectField}
     >
       {Object.keys(values).map((value, i) => {
         return <option key={i} value={values[value]}>
-          {value}
+          {formatEnumValue(value)}
         </option>
       })}
     </select>
@@ -153,17 +255,36 @@ const FieldRow = ({
   field,
   fieldData,
   i,
-  classes
+  classes,
+  dispatch
 }) => {
+  const [state, setState] = useState('')
+
+  function handleInputChange (e, tags = false) {
+    if (tags) {
+      setState(e)
+    } else {
+      setState(e.target.value)
+    }
+  }
+
+  useEffect(() => {
+    dispatch({
+      type: 'UPDATE',
+      value: state,
+      field: field
+    })
+  }, [state])
+
   const { type, repeated } = fieldData
   return <div className={classes.fieldContainer} key={i}>
     <span className={classes.fieldTitle}>
-      Field: {field} | Type: {type}
+      Field: {field} | Type: {repeated ? `Repeated` : null} {type}
     </span>
     {repeated ? <TagsInput
       placeholder={`${field}`}
       getTags={(tags) => {
-
+        handleInputChange(tags, true)
       }}
       allowSpaces={true}
       classes={classes}
@@ -171,12 +292,90 @@ const FieldRow = ({
       className={classes.inputField}
       placeholder={field.toLowerCase()}
       type={type}
+      value={state}
+      onChange={handleInputChange}
     />}
   </div>
 }
 
+const Publisher = ({
+  classes,
+  getMessage,
+  onSuccess,
+  onError,
+  mainnetExplorerUrl,
+  testnetExplorerUrl
+}) => {
+  const [wif, setWif] = useState('')
+  const [network, setNetwork] = useState('mainnet')
+
+  function handlePrivateKey(e) {
+    setWif(e.target.value)
+  }
+
+  function handleNetworkChange (e) {
+    setNetwork(e.target.value)
+  }
+
+  function _getMessage() {
+    return getMessage({wif, network})
+  }
+
+  return <div className={classes.publishContainer}>
+    <div className={classes.fieldContainer}>
+      <span className={classes.fieldTitle}>Private key (wif)</span>
+      <input
+        id={'wif'}
+        type={'text'}
+        onChange={handlePrivateKey}
+        value={wif}
+        className={classes.inputField}
+        placeholder={'private key (wallet import format)'}
+      />
+    </div>
+    <div className={classes.publishRow}>
+      <select
+        value={network}
+        onChange={handleNetworkChange}
+        className={classes.networkSelect}>
+        <option value={'mainnet'}>
+          mainnet
+        </option>
+        <option value={'testnet'}>
+          testnet
+        </option>
+      </select>
+      <div className={classes.walletButton}>
+        <WalletButton
+          text={'Create & Publish'}
+          wif={wif}
+          network={network}
+          getMessage={_getMessage}
+          onSuccess={onSuccess}
+          onError={onError}
+          mainnetExplorerUrl={mainnetExplorerUrl}
+          testnetExplorerUrl={testnetExplorerUrl}
+        />
+      </div>
+    </div>
+  </div>
+}
+
+function formatEnumValue (value) {
+  value = value.split('_')
+  if (value[1]) {
+    value = value[1].toLowerCase()
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  }
+  return value[0].toLowerCase()
+}
+
 RecordProto.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  descriptor: PropTypes.string.isRequired,
+  templateName: PropTypes.string.isRequired,
+  onSuccess: PropTypes.func,
+  onError: PropTypes.func,
 }
 
 export default withStyles(styles)(RecordProto)
