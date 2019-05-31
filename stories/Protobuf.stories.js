58687de6fd2856c765f2d6339c4249fa93e79836
orpheus <ryanachacon@gmail.com>
2019-05-31 11:27:12 -0700

    update templates in story

--- stories/Protobuf.stories.js
+++ stories/Protobuf.stories.js
@@ -77,20 +77,16 @@ storiesOf('Protobuf', module)
 
     const templates = [
       {
-        file_descriptor_set: 'CkAKB3AucHJvdG8iLQoBUBITCgtzdXBlclBvd2VycxgBIAMoCRITCgtsb3ZlQWZmYWlycxgCIAMoCWIGcHJvdG8z',
-        name: 'tmpl_2F29D8C0'
+        file_descriptor_set: 'CmcKGG9pcFByb3RvX3RlbXBsYXRlcy5wcm90bxISb2lwUHJvdG8udGVtcGxhdGVzIi8KAVASDAoEbmFtZRgBIAEoCRIMCgRzb2lsGAIgASgJEg4KBmNvbG9ycxgDIAMoCWIGcHJvdG8z',
+        name: 'tmpl_5D8DB85B'
       },
       {
-        file_descriptor_set: 'ClAKG29pcDVfcmVjb3JkX3RlbXBsYXRlcy5wcm90bxIVb2lwNS5yZWNvcmQudGVtcGxhdGVzIhIKAVASDQoFbXVzaWMYASABKAliBnByb3RvMw==',
-        name: 'tmpl_D5725A4B'
+        file_descriptor_set: 'CmYKB3AucHJvdG8SEm9pcFByb3RvLnRlbXBsYXRlcyI/CgFQEgwKBG5hbWUYASABKAkSFQoNZmF2b3JpdGVTcG9ydBgCIAEoCRIVCg1mYXZvcml0ZUZydWl0GAMgASgJYgZwcm90bzM=',
+        name: 'tmpl_348E9396'
       },
       {
-        file_descriptor_set: 'CmcKGG9pcFByb3RvX3RlbXBsYXRlcy5wcm90bxISb2lwUHJvdG8udGVtcGxhdGVzIi8KAVASDAoEbmFtZRgBIAEoCRILCgNhcnQYAiABKAkSDwoHd2Vic2l0ZRgDIAEoCWIGcHJvdG8z',
-        name: 'tmpl_4D44E6D1'
-      },
-      {
-        file_descriptor_set: 'CmwKG29pcDVfcmVjb3JkX3RlbXBsYXRlcy5wcm90bxIVb2lwNS5yZWNvcmQudGVtcGxhdGVzIi4KAVASDAoEbmFtZRgBIAEoCRILCgNhZ2UYAiABKA0SDgoGcG93ZXJzGAMgAygJYgZwcm90bzM=',
-        name: 'tmpl_DC6CA90C'
+        file_descriptor_set: 'CnkKCnR4aWQucHJvdG8SCG9pcFByb3RvIhgKBFR4aWQSEAoDcmF3GAEgASgMUgNyYXdCCloIb2lwUHJvdG9KMwoFEgMAAAAKCAoBDBIDAAAACggKAQISAwAAAAoJCgIEABIDAAAACgsKBAQAAgASAwAAAGIGcHJvdG8zCscBCgdwLnByb3RvEhJvaXBQcm90by50ZW1wbGF0ZXMaCnR4aWQucHJvdG8iTQoBUBIQCgRtYXNzGAEoA1IEbWFzcxIQCgRuYW1lGAIoCVIEbmFtZRIkCgVtb29ucxgDIAMoCzIOLm9pcFByb3RvLlR4aWRSBW1vb25zSk0KBRIDAAAACggKAQwSAwAAAAoICgECEgMAAAAKCQoCBAASAwAAAAoLCgQEAAIAEgMAAAAKCwoEBAACARIDAAAACgsKBAQAAgISAwAAAA==',
+        name: 'tmpl_D7309B2A'
       }
     ]
     return <ThemeWrapper>
